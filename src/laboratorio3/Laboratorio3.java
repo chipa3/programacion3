886e6f22cd1e1327e0a1d7f1eeceb8f85b9dbbbf
U. Mariano Gálvez <alumno@labs.academico>
2019-02-12 11:02:43 -0600

    agregados nuevos

--- src/laboratorio3/Laboratorio3.java
+++ src/laboratorio3/Laboratorio3.java
@@ -3,91 +3,123 @@ NOMBRE:JOSE ESTANISLAO LOPEZ TUBAC
 0901-17-12
  */
 package laboratorio3;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class Laboratorio3 {
     public static void main(String[] args) {
-      int dep[] = new int[5];
+      float dep[] = new float[5];
       Scanner lector = new Scanner(System.in);
-      String plani[][] = new String[10][6];
-      String sueldoBase,descuentos,percepciones,sueldo,departamento;
-      int intsueldobase,intdescuentos,intpercepciones,opcion,intsueldo = 0,intdep = 0;
-       do{  
-
+      String plani[][] = new String[10][10];
+      String ISR[][] = new String[10][3];
+      int intcodigo,intsueldobase,intdescuento,intbonificacion,intcomision,opcion,intsueldoliquido,intdep;
+      float fltigss,fltsueldoliquido,fltsumatoria,fltisr = 0,isr;
+      DecimalFormat df = new DecimalFormat("#.00");
+       do{   
              for(int j = 0;j <10;j++)
                     {
                     intdep = (int) (1 + (Math.random()*5));
-                    departamento = Integer.toString(intdep);  
-                    plani[j][0] = departamento;                    
-                    plani[j][1] = JOptionPane.showInputDialog(null,"PLANILLA DE SALARIOS\nINGRESE EL NOMBRE No."+(j+1));
+                    plani[j][0]= Integer.toString(intdep); 
+                    /*plani[j][1] = JOptionPane.showInputDialog(null,"PLANILLA DE SALARIOS\nINGRESE EL CODIGO DEL EMPLEADO No."+(j+1));*/
+                    plani[j][2] = JOptionPane.showInputDialog(null,"PLANILLA DE SALARIOS\nINGRESE EL NOMBRE DEL EMPLEADO No."+(j+1));
                     //INGRESO DE CANTIDADES ALEATOREAS A UNA VARIABLE DE TIPO INT
-                    intsueldobase = (int)(2500 + Math.random()*5000);
+                    intsueldobase = (int)((Math.random()*5000)+2500);
                     //CONVERSION DE UNA VARIABLE INT A UNA STRING
-                    sueldoBase = Integer.toString(intsueldobase);
-                    //ASIGNACION DE LA VARIABLE STRING A UNA COLUMNA EN LA MATRIZ
-                    plani[j][2] = sueldoBase;
-                     //INGRESO DE CANTIDADES ALEATOREAS A UNA VARIABLE DE TIPO INT
-                    intdescuentos = (int)(1 + Math.random()*2500);
-                    //CONVERSION DE UNA VARIABLE INT A UNA STRING
-                    descuentos = Integer.toString(intdescuentos);
-                    //ASIGNACION DE LA VARIABLE STRING A UNA COLUMNA EN LA MATRIZ
-                    plani[j][3] = descuentos;
-                     //INGRESO DE CANTIDADES ALEATOREAS A UNA VARIABLE DE TIPO INT
-                    intpercepciones = (int)(1+ Math.random()*2500);
-                     //CONVERSION DE UNA VARIABLE INT A UNA STRING
-                    percepciones = Integer.toString(intpercepciones);
-                    //ASIGNACION DE LA VARIABLE STRING A UNA COLUMNA EN LA MATRIZ
-                    plani[j][4] = percepciones;
-                    //OPERACION DE SUMA Y RESTA Y LA ASIGANACION A UNA VARIABLE DE TIPO INT
-                    intsueldo = intsueldobase + intpercepciones - intdescuentos;
-                     //CONVERSION DE UNA VARIABLE INT A UNA STRING
-                    sueldo = Integer.toString(intsueldo);
-                     //ASIGNACION DE LA VARIABLE STRING A UNA COLUMNA EN LA MATRIZ
-                  plani[j][5] =sueldo ;                
-                    }
-      //impresion de los vectores y arreglos
-      System.out.println("_________________________________________________________________________________________________________");
-      System.out.println("No.DEPARTAMENTO-------NOMBRES-------SUELDO BASE-------DESCUENTOS-------PERCEPCIONES-------SUELDO LIQUIDO");
+                    plani[j][3] = Integer.toString(intsueldobase);
+                    intbonificacion = (int)((Math.random()*500)+1);
+                    plani[j][4] = Integer.toString(intbonificacion);
+                    intcomision = (int)((Math.random()*500)+ 1);
+                    plani[j][5] = Integer.toString(intcomision);
+                    fltigss = (float) ((intsueldobase + intcomision )*0.0483);                
+                    plani[j][6] = String.valueOf(df.format(fltigss));
+                    intdescuento = (int)((Math.random()*500)+1);                   
+                    plani[j][7] = Integer.toString(intdescuento);
+                    
+                    if(intsueldobase >=2500 && intsueldobase <= 5000)
+                    {
+                    fltisr = (float) ((intsueldobase)*0.03);
+                    plani[j][8] = String.valueOf(df.format(fltisr));
+                    }else if(intsueldobase >5001 && intsueldobase <10000)
+                    {
+                     fltisr = (float) ((intsueldobase)*0.05); 
+                     plani[j][8] = String.valueOf(df.format(fltisr));
+                    }else if(intsueldobase >10001 && intsueldobase <100001)
+                    {
+                     fltisr = (float) ((intsueldobase)*0.10); 
+                     plani[j][8] = String.valueOf(df.format(fltisr));
+                    }                                     
+                    fltsueldoliquido = (intbonificacion + intsueldobase + intcomision-fltigss-intdescuento-fltisr);
+                    plani[j][9] = String.valueOf(df.format(fltsueldoliquido));
+                     }                 
+      //IMPRESION DE LA MATRIZ
+      System.out.println("__________________________________________________________________________________________________________________________________________________________________________________________________________________");
+      System.out.println("No.DEPARTAMENTO\t\tCODIGO\t\tNOMBRE\t\tSUELDO BASE\t\tBONIFICACION\t\tCOMISIONES\t\tIGSS\t\t\tDESCUENTOS J.\t\tISR\t\tSUELDO LIQUIDO");
       for(int i = 0; i <10;i++)
-      {
-      //IMPRESION EN PANTALLA DE COLUMNAS Y FILAS DE UNA MATRIZ Y UN VECTOR     
-      System.out.println("\t"+plani[i][0]+"\t\t"+ plani[i][1]+"\t\tQ."+plani[i][2]+"\t\tQ."+plani[i][3]+"\t\tQ."+plani[i][4]+"\t\t\tQ."+plani[i][5]);
-      System.out.println("_________________________________________________________________________________________________________");   
-      } 
-      int dep1 = 0,dep2 = 0,dep3=0,dep4=0,dep5=0,sumatoria;
+      {    
+       System.out.println("\t"+plani[i][0]+"\t\t"+ plani[i][1]+"\t\t"+plani[i][2]+"\t\tQ."+plani[i][3]+"\t\t\tQ."
+               + ""+plani[i][4]+"\t\t\tQ."+plani[i][5]+"\t\t\tQ."+plani[i][6]+"\t\tQ."+plani[i][7]+"\t\t\tQ."+plani[i][8]+"\t\tQ."+plani[i][9]);
+       System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________");
+      }        
+     System.out.println("\n");
       for(int i = 0; i<10;i++)
-      {
-          //sumatoria va a guardar el valor que esta en la matriz pero convertida a entero
-        sumatoria   = Integer.parseInt(plani[i][5]);
+      { 
+      fltsumatoria = Float.parseFloat(plani[i][9]);
       if(plani[i][0].equals("1"))
       {
-          //en la posicion 0 se guardara lo que tiene el vector mas lo que tiene la matriz, asi repetidamente mediante se cumpla el if
-        dep[0]= dep[0] + sumatoria;
+         dep[0] = dep[0] + fltsumatoria;
       }
-       if(plani[i][0].equals("2"))
+      if(plani[i][0].equals("2"))
       {
-      dep[1]= dep[1] + sumatoria;
+          dep[1] = dep[1] + fltsumatoria;
       }
-        if(plani[i][0].equals("3"))
+       if(plani[i][0].equals("3"))
       {
-       dep[2]= dep[2] + sumatoria;
+         dep[2] = dep[2] + fltsumatoria;
       }
-         if(plani[i][0].equals("4"))
+      if(plani[i][0].equals("4"))
       {
-       dep[3]= dep[3] + sumatoria;
+        dep[3] = dep[3] + fltsumatoria;
       }
-          if(plani[i][0].equals("5"))
+      if(plani[i][0].equals("5"))
       {
-       dep[4]= dep[4] + sumatoria;
+          dep[4] = dep[4] + fltsumatoria;
       }
       }
-      for(int i = 0; i <=4;i++)
-      {
-       System.out.println("DEPARTAMENTO "+(i+1)+" = "+dep[i]);
-      }
+       System.out.println("TOTAL DE GASTO POR DEPARTAMENTO: \n");
+       for(int i = 0;i<=4;i++)
+       {
+           System.out.println("DEPATAMENTO "+(i+1)+":    "+dep[i]+"\n");
+       }
+       for(int i = 0; i <10; i++)
+       {
+       isr = Integer.parseInt(plani[i][3]);
+        if(isr >= 2500 && isr<=5000)
+        {
+          ISR[i][0] = plani[i][2];
+          ISR[i][1] = "3%";
+          ISR[i][2] = plani[i][8];
+        }
+        else if(isr >5000 && isr <=10000)
+        {
+          ISR[i][0] = plani[i][2];
+          ISR[i][1] = "5%";
+          ISR[i][2] = plani[i][8];
+        }
+        else if(isr >=  10000 && isr <= 100000)
+        {
+            ISR[i][0] = plani[i][2];
+            ISR[i][1] = "10%";
+            ISR[i][2] = plani[i][8];
+        } 
+       }
+       System.out.println("PORCENTAJE DE ISR POR EMPLEADO: \n");
+       for(int i = 0;i<10;i++)
+       {
+           System.out.println("EMPLEADO "+(i+1)+":"+ISR[i][0]+"\t\t\tPAGA:"+ISR[i][1]+"\t\t\tEL ISR ES:"+ISR[i][2]);
+       }
        //MENU PARA EL CICLO WHILE
-       System.out.println("¿DESEA VOLVER A INGRESAR LOS NOMBRES?\n1.si\n2.no");
-       opcion = lector.nextInt();       
+       System.out.println("\n¿DESEA VOLVER A INGRESAR LOS NOMBRES?\n1.si\n2.no");
+       opcion = lector.nextInt();        
        }while(opcion!=2);
     }
 }
