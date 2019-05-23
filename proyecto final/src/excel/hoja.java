fbd675121308fe56ab639d1480ec00b3d7433828
chipa3 <chipa_124578@outlook.com>
2019-05-22 18:57:03 -0600

    limpiar

--- proyecto final/src/excel/hoja.java
+++ proyecto final/src/excel/hoja.java
@@ -304,7 +304,7 @@ public class hoja extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 968, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 968, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
